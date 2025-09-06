f4616bff3bb7bc0245200c5c13c939227414b307
kiruthikhaN <nkiruthikha93@gmail.com>
2025-09-06 23:51:46 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './Navbar'
+import Democard from './Card'
+function App(){
+  return(
+    <>
+    {/* <Navbar></Navbar> */}
+     <h1 className="title">Hy this is my first component!</h1>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, itaque!</p>
+
+       <Democard />
+        <Democard />
+
+
+    </>
+
+   
+  ) 
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+       <>
+       <div className="card">
+        <img src="https://plus.unsplash.com/premium_photo-1756066699592-eb346b61309b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, architecto.</p>
+       </div>
+       </> 
+    )
+}
+export default Card;
